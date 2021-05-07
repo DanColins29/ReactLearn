e65463063a376dc09e9efe22103fe448f93be28f
Dan <danielcolins@multitask.net.br>
2021-05-07 18:10:52 -0300

    No version
    My firsts steps about react framework

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+      <h1>Hello World!</h1>
+      </div>
+      
+      <div>
+        <h4>Título</h4>
+        <p>Jghakdsgkjsagahsggadgsgdasdhashshshksab agdksahdjahsdhasdkash</p>
+        <p>jdhcjkhcshjkshjdhasjh jksdjkashdjaksh</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
